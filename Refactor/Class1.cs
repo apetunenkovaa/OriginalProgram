3b80bc1506b58b66d78a6069dc31f239768c2710
apetunenkovaa <apetunenkovaa@mail.ru>
2022-12-27 02:24:25 +0300

    Добавьте файлы проекта.

---
+++ Refactor/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Refactor
+{
+    class Class1
+    {
+        public static string txt;
+        public static void remov(int k)
+        {
+            txt = txt.Remove(k);
+            Console.WriteLine($"Измененная строка: {txt}");
+        }
+    }
+}
---
+++ Refactor/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Refactor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Console.Write(" ЗАДАЧА 1.Прибавить 10 к введенному числу");
+           Console.WriteLine("\nВведите число и нажмите enter");
+           double a = Convert.ToDouble(Console.ReadLine());
+          
+
+           double sum = a + 10;
+      
+           Console.WriteLine($Число равно - {sum}"); */
+
+            Console.WriteLine("Задача №2");
+            Console.WriteLine("Введите строку: ");
+            Class1.txt = Console.ReadLine();
+            Console.WriteLine("Введите индекс: ");
+            int k = Convert.ToInt32(Console.ReadLine());
+            Class1.remov(k);
+
+
+
+
+           
+
+
+        }
+    }
+}
